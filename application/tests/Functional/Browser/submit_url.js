cf6422b32de7620331242e9e73f30bae54589d3c
tomasb <tomas.bareikis@nfq.lt>
2014-09-23 19:32:47 +0300

    change and uncomment test

--- application/tests/Functional/Browser/submit_url.js
+++ application/tests/Functional/Browser/submit_url.js
@@ -1,12 +1,22 @@
 require('../parameters');
+
 casper.test.begin('Url submitting', 2, function suite(test) {
-    casper.start('http://google.lt/', function() {
-// search for 'casperjs' from google form
-        this.fill('form[action="/search"]', { q: 'casperjs' },
-            true);
-    });casper.then(function () {
+    var slug = Math.floor((Math.random() * 1000000) + 1);
+    var url = 'http://www.random.org/integers/?num=100&min=1&max=' + slug + '&col=5&base=10&format=html&rnd=new';
+
+    casper.start(document.app.parameters.base_url + '/crawler/crawl', function () {
+        test.assertExists('form', 'Form must exist');
+        this.fill('form', {url: url}, true);
+    });
+
+    casper.thenOpen(document.app.parameters.base_url + '/crawler/index', function () {
+        test.assertExists('.page-list a[href="' + url + '"]', 'Link must exist');
+    });
+
+    casper.then(function () {
         this.capture('google.png');
     });
+
     casper.run(function () {
         test.done();
     });
